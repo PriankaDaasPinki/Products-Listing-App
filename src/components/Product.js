6aba41148d706a3582a8d16a7fe58dbb002a271a
PriankaDaasPink <priankadas55555@gmail.com>
2024-12-03 12:42:51 +0600

    eslint

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Card } from 'react-bootstrap';
+// import { Card } from 'react-bootstrap';
 import State from './State';
 
 const Product = ({ item }) => {
